ebe14c6e2a7f6e5e88e6424805d6604a09d1b502
Jazzy <jasmina.gasper@gmail.com>
2016-03-30 18:04:10 +0100

    buttons work

--- interface.js
+++ interface.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat._temperature);
+  $('#up').click(function() {
+    thermostat.up();
+  $('#temperature').text(thermostat._temperature);
+  });
+  $('#down').click(function() {
+    thermostat.down();
+  $('#temperature').text(thermostat._temperature);
+  });
+  $('#reset').click(function() {
+    thermostat.reset();
+  $('#temperature').text(thermostat._temperature);
+  });
+  $('#energySavingButton').click(function() {
+    thermostat.powerSavingSwitch();
+  });
 });
