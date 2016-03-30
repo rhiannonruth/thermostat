711abdc488eb4b4e02d795b2d434bebcedc50eb3
Jazzy <jasmina.gasper@gmail.com>
2016-03-30 13:51:58 +0100

    temperature down and min temp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -11,10 +11,28 @@ describe('Thermostat', function(){
   });
 
   describe('up button', function(){
-    
+
     it('increases temp', function(){
       thermostat.up();
       expect(thermostat.temperature()).toEqual(21)
       });
   })
+
+  describe('down button', function(){
+
+    it('decreases temp', function(){
+      thermostat.down();
+      expect(thermostat.temperature()).toEqual(19)
+      });
+
+    it('raises error when tries to go below min temp', function(){
+      for(var i=1; i <= 10; i++){
+        thermostat.down();
+      };
+      expect(function(){
+        thermostat.down();
+      }).toThrowError('too damn cold!');
+    });
+  });
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -9,3 +9,10 @@ Thermostat.prototype.temperature = function() {
 Thermostat.prototype.up = function() {
   this._temperature += 1;
 }
+
+Thermostat.prototype.down = function() {
+  if (this._temperature === 10){
+    throw new Error('too damn cold!');
+  }
+  this._temperature -= 1;
+}
